5449b315d0f55b202c312270f6e9ba1bbc94b288
SergeyAverin <ct.averin@yandex.ru>
2023-09-25 20:22:29 +0600

    added site bookmark

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <h1>APP</h1>
+  <SiteBookmark href="https://www.deepl.com/translator#en/ru/Site%20Bookmark" text="tt" />
 </template>
 
 <script>
+import SiteBookmark from './components/SiteBookmark.vue';
+
 export default {
   name: 'App',
   components: {
-    
+    SiteBookmark,
   }
 }
 </script>
 
 <style>
+  body {
+    background: #101211;
+    color: #fff;
+  }
 </style>
---
+++ src/components/SiteBookmark.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="site-bookmark">
+        <a :href='href'>{{ text }}</a>
+        <img :src="faviconUrl" :alt="text" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        href: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
+
+<style>
+.site-bookmark {
+    width: 169px;
+    height: 169px;
+    border-radius: 15px;
+    background: #1E1E1E;
+}
+</style>
