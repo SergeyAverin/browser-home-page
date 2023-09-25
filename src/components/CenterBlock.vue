c6d666f78be01adc518a0567c2198cbafdcb2894
SergeyAverin <ct.averin@yandex.ru>
2023-09-25 21:31:28 +0600

    added center block component

---
+++ src/components/CenterBlock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="center-block"
+    :style="{
+      justifyContent: horizontal  && 'center',
+      alignItems: vertikal && 'center',
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "CenterBlock",
+  props: {
+    horizontal: { type: Boolean },
+    vertikal: { type: Boolean },
+  },
+};
+</script>
+<style>
+.center-block {
+  display: flex;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
+</style>
